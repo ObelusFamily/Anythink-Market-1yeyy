013b45d9a3a22d8da1c20e65a1cc4eca312e7fc6
Rjt5412 <rajat.malviya5412@gmail.com>
2022-08-09 00:05:16 +0530

    Fix for #2: Format fix

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,9 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image ? { uri: item.image } : require('../imgs/placeholder.png')}
+        src={
+          item.image ? { uri: item.image } : require("../imgs/placeholder.png")
+        }
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
